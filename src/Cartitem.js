cddcbba078a2a40ec2254b3d76cbd8232620ad22
Tanmay <tanmaygpro@gmail.com>
2021-05-17 16:19:10 +0530

    add increase qty function

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -10,6 +10,9 @@ class CartItem extends React.Component {
             img: ''
         }
     }
+    increaseQuantity = () => {
+        console.log(this.state)
+    }
     render(){
         const {price, title, qty} = this.state;
         return (
@@ -35,7 +38,9 @@ class CartItem extends React.Component {
                     }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="increase" className="action-icons" src="https://img-premium.flaticon.com/png/512/1828/1828926.png?token=exp=1621221536~hmac=4d4f45e2815e46d2d38d683732f93224" />
+                        <img alt="increase" className="action-icons" src="https://img-premium.flaticon.com/png/512/1828/1828926.png?token=exp=1621221536~hmac=4d4f45e2815e46d2d38d683732f93224" 
+                        onClick={this.increaseQuantity}
+                        />
                         <img alt="decrease" className="action-icons" src="https://img-premium.flaticon.com/png/512/992/992683.png?token=exp=1621221483~hmac=82f77bd771178b126f13c0bcaeefae02" />
                         <img alt="delete" className="action-icons" src="https://img-premium.flaticon.com/png/512/1345/1345874.png?token=exp=1621221572~hmac=0f94eb245e95fead294d7b7691cd65f4" />
                     </div>
