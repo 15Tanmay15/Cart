d138e297c8a98c601bd5f5b1b56c01ddc82f7295
Tanmay <tanmaygpro@gmail.com>
2021-05-17 17:36:49 +0530

    qty increase and decrease

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -11,7 +11,26 @@ class CartItem extends React.Component {
         }
     }
     increaseQuantity = () => {
-        console.log(this.state)
+        // setState form 1
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // })
+        // setState form 2
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1
+            }
+        });
+    }
+
+    decreaseQuantity = () => {
+        this.setState((prevState) => {
+            if(prevState.qty > 0){
+            return {
+                qty: prevState.qty - 1
+            }
+        }
+        });
     }
     render(){
         const {price, title, qty} = this.state;
@@ -41,7 +60,9 @@ class CartItem extends React.Component {
                         <img alt="increase" className="action-icons" src="https://img-premium.flaticon.com/png/512/1828/1828926.png?token=exp=1621221536~hmac=4d4f45e2815e46d2d38d683732f93224" 
                         onClick={this.increaseQuantity}
                         />
-                        <img alt="decrease" className="action-icons" src="https://img-premium.flaticon.com/png/512/992/992683.png?token=exp=1621221483~hmac=82f77bd771178b126f13c0bcaeefae02" />
+                        <img alt="decrease" className="action-icons" src="https://img-premium.flaticon.com/png/512/992/992683.png?token=exp=1621221483~hmac=82f77bd771178b126f13c0bcaeefae02" 
+                        onClick={this.decreaseQuantity}
+                        />
                         <img alt="delete" className="action-icons" src="https://img-premium.flaticon.com/png/512/1345/1345874.png?token=exp=1621221572~hmac=0f94eb245e95fead294d7b7691cd65f4" />
                     </div>
                 </div>
