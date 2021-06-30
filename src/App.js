6366e9443acfbd17e53ee26377454d9373981383
Tanmay <tanmaygpro@gmail.com>
2021-06-30 11:58:14 +0530

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,66 +1,115 @@
 import Cart from './Cart';
 import Navbar from './Navbar';
 import React from 'react';
+import firebase from 'firebase';
 
 class App extends React.Component{
   constructor (){
     super();
     this.state={
-        products: [
-            {
-                price: 999,
-                title: 'Mobile Phone',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1516&q=80',
-                id: 1
-            },
-            {
-                price: 99,
-                title: 'Watch',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
-                id: 2
-            },
-            {
-                price: 9999,
-                title: 'Laptop',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
-                id: 3
-            },
-        ]
+        products: [],
+        loading: true
     }
+    this.db = firebase.firestore();
 }
+
+componentDidMount(){
+    // firebase
+    //     .firestore()
+    //     .collection('products')
+    //     .get()
+    //         .then((snapshot) => {
+    //             console.log(snapshot);
+
+    //             snapshot.docs.map((doc) => {
+    //                 console.log(doc.data())
+    //             });
+
+    //             const products = snapshot.docs.map((doc)=>{
+    //                 const data = doc.data();
+
+    //                 data['id'] = doc.id;
+    //                 return data;
+    //             })
+
+    //             this.setState({
+    //                 products,
+    //                 loading: false
+    //             })
+    //         })
+
+    firebase
+        .firestore()
+        .collection('products')
+        .onSnapshot((snapshot) => {
+            console.log(snapshot);
+
+            snapshot.docs.map((doc) => {
+                console.log(doc.data())
+            });
+
+            const products = snapshot.docs.map((doc)=>{
+                const data = doc.data();
+
+                data['id'] = doc.id;
+                return data;
+            })
+
+            this.setState({
+                products,
+                loading: false
+            })
+        })
+}
+
 handleIncreaseQuantity = (product) => {
     const { products } = this.state;
     const index = products.indexOf(product);
 
-    products[index].qty+=1;
+    // products[index].qty+=1;
 
-    this.setState({
-        products
+    // this.setState({
+    //     products
+    // })
+
+    const docRef = this.db.collection('products').doc(products[index].id);
+
+    docRef.update(
+        {
+            qty: products[index].qty + 1
+        }
+    ).then(()=>{
+        console.log('Updated successfully')
+    }).catch((error)=>{
+        console.log('Error: ', error)
     })
 }
 handleDecreasingQuantity = (product) => {
     const { products } = this.state;
     const index = products.indexOf(product);
 
+    const docRef = this.db.collection('products').doc(products[index].id);
+
     if(products[index].qty == 0)
     return;
 
-    products[index].qty -=1;
-
-    this.setState({
-        products
+    docRef.update({
+            qty: products[index].qty - 1
+    }).then(()=>{
+        console.log('Updated successfully')
+    }).catch((error)=>{
+        console.log('Error: ', error)
     })
 }
 handleDeleteProduct = (id) => {
     const { products } = this.state;
 
-    const items = products.filter((item) => item.id !== id);
+    const docRef = this.db.collection('products').doc(id);
 
-    this.setState({
-        products: items
+    docRef.delete().then(()=>{
+        console.log('Deleted successfully')
+    }).catch((error)=>{
+        console.log('Error: ', error)
     })
 }
 getCartCount = () => {
@@ -82,15 +131,36 @@ getCartTotal = () => {
     })
     return price;
 }
+
+    addProduct = () => {
+        this.db
+            .collection('products')
+            .add({
+                img: 'https://www.lg.com/in/images/washing-machines/md07518468/gallery/FHV1207ZWB-Washing-Machines-Right-View-D-06.jpg',
+                price: 18000,
+                qty: 3,
+                title: 'washing machine'
+            })
+            .then((docRef) => {
+                console.log('Product has been added', docRef)
+            })
+            .catch((error) => {
+                console.log('Error : ', error)
+            })
+    }
     render(){
-        const {products} = this.state;
+        const {products, loading} = this.state;
         return (
         <div className="App">
       <Navbar count={this.getCartCount()}/>
+      {/* <button onClick={this.addProduct} style={{
+          padding: 10, fontSize: 15
+      }}>Add a product</button> */}
       <Cart 
       products = {products}
       onIncreaseQuantity={this.handleIncreaseQuantity} onDecreaseQuantity={this.handleDecreasingQuantity} onDeleteProduct={this.handleDeleteProduct}
       />
+      {loading && <h1>Loading Products ...</h1>}
       <div style= {
           {padding: 10, fontSize: 20, fontFamily: 'monospace'}
       }>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDynGrDc94G7HIXURBYGus9JYO7hp8IJAE",
+  authDomain: "cart-88f3a.firebaseapp.com",
+  projectId: "cart-88f3a",
+  storageBucket: "cart-88f3a.appspot.com",
+  messagingSenderId: "1069850293309",
+  appId: "1:1069850293309:web:f602f66823b53a4e25e654"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 ReactDOM.render(
